82d413c7ede4626afc418438c1ea212d94f7dcba
Greg Rose <gregrose628@gmail.com>
2018-02-23 21:06:15 -0500

    fixed WeatherIcon component class name

--- app/components/WeatherIcon.js
+++ app/components/WeatherIcon.js
@@ -3,7 +3,7 @@ import { createIconSetFromFontello } from 'react-native-vector-icons';
 import fontelloConfig from '../config.json';
 const Icon = createIconSetFromFontello(fontelloConfig);
 
-export default class CurrentWeather extends Component {
+export default class WeatherIcon extends Component {
   render() {
     return (
       <Icon name={this.props.name}
